078be087875b2c5d078ecc2f6ef5cf02ca9bf947
MODPlayYT <58366028+MODPlayYT@users.noreply.github.com>
2022-05-01 21:44:45 +0300

    Initial commit

---
+++ src/js/01_main.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function(){
+	// Класс active для активного пункта меню
+    // var activeUrl = window.location.pathname.slice(1),
+	// 	menuItem = document.querySelector('a[href="'+activeUrl+'"]');
+	// 	console.log(activeUrl);
+	// 	console.log(menuItem);
+	// 	menuItem.classList.add('active');
+
+	// Кнопка меню
+	// let menus = document.querySelectorAll('.hamburger-menu');
+	// for (let i = 0; i < menus.length; i++) {
+	// 	menus[i].addEventListener('click', function() {
+	// 		menus[i].classList.toggle('open');
+	// 	});
+	// }
+
+	// // Слайдер баннеров на главной 
+	// const swiperBanner = new Swiper('.banner', {
+	// 	slidesPerView: 1, // Слайдов на экран
+	// 	spaceBetween: 10, // Отступы между слайдами
+	// 	centeredSlides: true, // Центровка слайдов
+	// 	loop: true, // Бесконечная прокрутка
+	// 	lazy: true, // Ленивая загрузка
+	// 	speed: 300, // Скорость анимации
+	// 	parallax: true, // Параллакс эффект
+    //     autoplay: { // Автоплей
+	// 		delay: 8000, // Время автоплея
+	// 		disableOnInteraction: false, // Отключение взаимодействия со слайдером при автоплее
+	// 		pauseOnMouseEnter: true, // Остановка автоплея при фокусе
+	// 	},
+	// });
+
+});
